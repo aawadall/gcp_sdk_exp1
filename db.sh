5e61a80d841fd5c027f8b92dbfd17caae53043f2
aawadall <aawadall@ualberta.ca>
2019-07-20 11:49:52 -0400

    set password

--- db.sh
+++ db.sh
@@ -1,5 +1,6 @@
 # Variables
 INSTANCE_NAME=blog-db
+ROOT_USER=root
 ROOT_PASSWORD=simplesimple # I know, this is for testing
 ZONE=us-central1-a
 DATABASE_VERSION=MYSQL_5_7
@@ -11,4 +12,7 @@ gcloud sql instances delete $INSTANCE_NAME --quiet
 gcloud sql instances create $INSTANCE_NAME \
     --zone=$ZONE \
     --tier=$TIER 
+
+# Set Root Password
+gcloud sql users set-password $ROOT_USER --instance=$INSTANCE_NAME --password $ROOT_PASSWORD     
 # Create Database 
