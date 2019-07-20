0b9cda50c5ba5c20a529b4c4b0f65bd926c8988f
aawadall <aawadall@ualberta.ca>
2019-07-20 15:48:18 -0400

    create db user

--- db_users.sh
+++ db_users.sh
@@ -6,7 +6,11 @@ ZONE=us-central1-a
 DATABASE_VERSION=MYSQL_5_7
 TIER=D4
 
-
+DB_USER=blogdbuser
+PASSWORD=simplesimple
 
 # Set Root Password
 gcloud sql users set-password $ROOT_USER --host % --instance $INSTANCE_NAME --password $ROOT_PASSWORD     
+
+# Create Blog DB User
+gcloud sql users create $DB_USER --host % --password $PASSWORD --instance $INSTANCE_NAME 
