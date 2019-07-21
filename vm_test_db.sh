b1da23aaa9a166c908a96102a1a5c5621206e391
aawadall <aawadall@ualberta.ca>
2019-07-20 20:58:51 -0400

    modified php code

--- vm_test_db.sh
+++ vm_test_db.sh
@@ -17,17 +17,17 @@ echo "VM IP = [$EXT_IP]"
 <body>
 <h1>Sample Blog Page</h1>
 <?php
-$dbserver = "$EXT_IP";
-$dbuser = "blogdbuser";
-$dbpassword = "simplesimple";
+\$dbserver = "$EXT_IP";
+\$dbuser = "blogdbuser";
+\$dbpassword = "simplesimple";
 
-$conn = new mysqli($dbserver, $dbuser, $dbpassword);
+\$conn = new mysqli(\$dbserver, \$dbuser, \$dbpassword);
 
-if (mysqli_connect_error()) {
-    echo ("Database connection failed: " . mysqli_connect_error() );
-} else {
-    echo ("Database connection successful");
-}
+// Check connection
+if (\$conn->connect_error) {
+    die("Connection failed: " . \$conn->connect_error);
+} 
+echo "Connected successfully";
 ?>
 </body></html>
 EOF
